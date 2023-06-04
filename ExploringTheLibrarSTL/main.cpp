7c1fb7e0f1dd00c4a4fa1490887b22a6637a0aa4
KANTNOLI <zahargysev02@gmail.com>
2023-06-04 17:42:30 +0300

    Lerning list

--- ExploringTheLibrarSTL/main.cpp
+++ ExploringTheLibrarSTL/main.cpp
@@ -6,12 +6,14 @@ using namespace std;
 int main() {
 	setlocale(LC_ALL, "RUS");
 
-	list<int> myList = {12, 24, 7};
-	myList.pop_front();
-	myList.pop_back();
-
+	list<int> myList = {12, 12, 12, 42, 12, 24, 12, 7};
+	
 	auto it = myList.begin();
-	myList.sort();
+	
+	advance(it, 5);
+
+	myList.assign(3, 122121);
+
 	for (auto i = myList.begin(); i != myList.end(); i++)
 	{
 		cout << *i << endl;
