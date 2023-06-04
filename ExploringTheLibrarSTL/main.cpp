51ff33a40a2da97d0ebd4569df6a9dca3940faa2
KANTNOLI <zahargysev02@gmail.com>
2023-06-04 17:32:02 +0300

    part 1 STL - LIST

--- ExploringTheLibrarSTL/main.cpp
+++ ExploringTheLibrarSTL/main.cpp
@@ -6,7 +6,17 @@ using namespace std;
 int main() {
 	setlocale(LC_ALL, "RUS");
 
+	list<int> myList = {12, 24, 7};
+	myList.pop_front();
+	myList.pop_back();
+
+	auto it = myList.begin();
+	myList.sort();
+	for (auto i = myList.begin(); i != myList.end(); i++)
+	{
+		cout << *i << endl;
+	}
 
 
 	return 0;
-}
+} 
